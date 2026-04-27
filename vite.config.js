1e89f426bc5736387e82c1c2fada1c1854a48c41
IlanAvigdor <ilanavigdor@gmail.com>
2026-04-27 23:29:44 +0300

    Cleanup: removed node_modules and other unnecessary files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // github pages usually needs a base path if it's not a custom domain
-  // base: '/Tasks/', 
+  base: '/Tasks/',
 });
